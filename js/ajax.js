7730305f3a0ea1a11b57281188ff625b2f0e450b
Eric S <ydrive65@yahoo.com>
2017-11-16 11:19:49 -0500

    end of bootcamp

--- js/ajax.js
+++ js/ajax.js
@@ -11,6 +11,14 @@ $().ready(function () {
         }
     );
 
+    $.getJSON(
+        "http://localhost:49315/Students/List",
+        function (students)
+        {
+            console.log(students);
+        }
+    );
+
     $("button").click(function () {
         console.log(test);
         updateTime(test);
@@ -29,5 +37,29 @@ $().ready(function () {
         {
             console.log(vendor["Name"], vendor["Email"], vendor["Phone"]);
         }
+    }
+
+    $("#addbtn").click(function () {
+        var student = getStudentData();
+
+        $.post("http://localhost:49315/Students/Add", student, function (result) {
+            console.log(result);
+        });
+    });
+
+    function getStudentData()
+    {
+        var student = {
+            Id: 0,
+            FirstName: "FirstName",
+            LastName: "LastName",
+            SAT: 1600,
+            GPA: 4.0,
+            Email: "email",
+            Phone: "phone",
+            MajorId: 1
+        };
+
+        return student;
     }
 });
